00e7e34b2a6af644d2803e9d6a29bc1a8b8ff69d
carburto <carburto@ucsc.edu>
2025-10-03 17:22:59 -0700

    Add files via upload

---
+++ cj1-2.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def main():
+	a = 2.5
+	b = 10.1
+	c = 6
+	d = 5
+
+	print("a+b = ", a+b)
+	print(type(a+b))
+	print("c-d = ", c-d)
+	print(type(c-d))
+	print("a*c = ", a*c)
+	print(type(a*c))
+
+
+if __name__=="__main__":
+	main()
