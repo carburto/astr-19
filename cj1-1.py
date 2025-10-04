742a59ce5d59f2089f7a2bd706de78889726e4b6
carburto <carburto@ucsc.edu>
2025-10-03 17:02:58 -0700

    Add files via upload

---
+++ cj1-1.py
@@ -0,0 +1,20 @@
+#This program will write about me
+
+
+#This function tells the computer to print information
+def PrintInfo():
+	print("Cash Burton")
+	print("he/him")
+	print("Favorite movie: Godzilla Minus One")
+	print("Favorite food: salmon sushi")
+
+#This defines our main
+#function for our program
+def main():
+	PrintInfo()
+
+#When we run the program
+#this executes first
+#if the main() function exists, run it
+if __name__=="__main__":
+	main()
